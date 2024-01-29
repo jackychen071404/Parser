7173f12960d530d29aa510518864deae63b25c5c
Jacky Chen <jchen071404@gmail.com>
2024-01-29 18:48:48 +0000

    added more conditionals

--- myshell_parser.c
+++ myshell_parser.c
@@ -25,16 +25,66 @@ struct pipeline *pipeline_build(const char *command_line) {
   int index = 1;
   int i = 0;
 
-  while(command_line[i]!='\0')
+  while(command_line[i]!='\0') //empty
     {
       if(command_line[i] == ' ' || command_line[i] == '\n' || command_line[i] == '\t')
 	i++;
-      if(current->redirect_out_path != NULL)
+      else if(command_line[i] == '>')
+	{
+	  i++;
+	  if(current->redirect_out_path != NULL)
 	{
 	  pipeline_free(pipe);
 	  return NULL;
 	}
-}
+	  char *out = malloc(MAX_LINE_LENGTH); //memory for redirect_out 
+	  while(command_line[i]==' ' || command_line[i] == '\n' || command_line[i] == '\t')
+	    i++;
+	  if(command_line[i] == '&' || command_line[i] == '>' || command_line[i] == '<' || command_line[i] == '|')
+	{
+	  pipeline_free(pipe);
+	  return NULL;
+	}
+	  int j = 0;
+	  while(command_line[i] != ' ' && command_line[i] != '\t' && command_line[i] != '\n' && command_line[i] != '&' &&command_line[i] && '>' != command_line[i] && '<' && command_line[i] != '|')
+	{
+	  out[j] = command_line[i];
+	  i++;
+	  j++;
+	}
+      current->redirect_out_path = out;
+	}
+      else if(command_line[i] == '&') //parser in background
+	{
+	  i++;
+	  pipe->is_background = 1;
+        }
+    }
+    else if(command_line[i] == '<')
+      {
+	i++;
+	if(current->redirect_in_path != NULL)
+	  {
+	    pipeline_free(pipe);
+	    return NULL;
+	  }
+	while(command_line[i]==' ' || command_line[i] == '\n' || command_line[i] == '\t')
+	  i++;
+	if(command_line[i] == '&' || command_line[i] == '>' || command_line[i] == '<' || command_line[i] == '|')
+	  {
+	    pipeline_free(pipe);
+	    return NULL;
+	  }
+	char *in = malloc(MAX_LINE_LENGTH);
+	int j = 0;
+	while(command_line[i] != ' ' && command_line[i] != '\t' && command_line[i] != '\n' && command_line[i] != '&' && command_line[i] != '>' && command_line[i] != '<' && command_line[i] != '|')
+	  {
+	    in[j] = command_line[i];
+	    j++;
+	    i++;
+	  }
+	current->redirect_in_path = in;
+      }
 
 void pipeline_free(struct pipeline *pipeline)
 {
@@ -47,9 +97,9 @@ void pipeline_free(struct pipeline *pipeline)
 	}
       free(current->redirect_in_path); 
       free(current->redirect_out_path);
-      struct pipeline_command *nextcommand = current->next;
+      struct pipeline_command *next_command = current->next;
       free(current); //this frees the fields, redirects and next
-      current = nextcommand; //move to the next one
+      current = next_command; //move to the next one
     }
   free(pipeline); //free th whole pipelin
 
