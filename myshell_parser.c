0a742597891b6cc5fcd99506f725f6dd5b097fb2
Jacky Chen <jchen071404@gmail.com>
2024-01-29 00:49:27 +0000

    added a comment

--- myshell_parser.c
+++ myshell_parser.c
@@ -8,7 +8,7 @@ struct pipeline *pipeline_build(const char *command_line)
   // TODO: Implement this function
   struct pipeline *pipe = malloc(sizeof(struct pipeline)); //allocate memory for a pipeline struct
   if (pipe == NULL) {
-    perror("Error for allocation");
+    perror("Error for allocation"); //print a failed message for failure to allocate memory
     exit(EXIT_FAILURE);
     }
   return NULL;
