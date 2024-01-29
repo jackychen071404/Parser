3a90f47e87b0725cb5b6a179afbe67a6a9c654ab
Jacky Chen <jchen071404@gmail.com>
2024-01-29 18:23:13 +0000

    initialize first, next, and redirect pipes

--- myshell_parser.c
+++ myshell_parser.c
@@ -4,7 +4,23 @@
 #include <string.h>
 
 struct pipeline *pipeline_build(const char *command_line) {
+  struct pipeline *pipe = malloc(sizeof(struct pipeline));
+  pipe->is_background = 0;
+  pipe->commands = NULL;
 
+  struct pipeline_command *first = malloc(sizeof(struct pipeline_command));
+  for(int i = 0; i < MAX_ARGV_LENGTH; i++)
+    {
+      first->command_args[i] = NULL;
+    }
+  first->next = NULL;
+  first->redirect_in_path = NULL;
+  first->redirect_out_path = NULL;
+
+  struct pipeline_command *current = NULL;
+
+  pipe->commands = first;
+  current = first;
 }
 
 void pipeline_free(struct pipeline *pipeline)
